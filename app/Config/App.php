d11829d99b6578c947e78681d16e07868ea0cf8a
Amit <amitnale2009@gmail.com>
2022-05-30 11:37:45 +0530

    change localhost to url

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/trial/';
+    public $baseURL = 'https://codeigniter4-crud.herokuapp.com/';
 
     /**
      * --------------------------------------------------------------------------
